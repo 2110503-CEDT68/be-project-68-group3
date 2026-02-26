00bdaafab2cc00b9ad9e51bc239d630f6eae8194
Napat Putsorn <napatput31@hotmail.com>
2026-02-26 22:32:54 +0700

    Fixed models/interviews.js (again T-T)

--- models/Interview.js
+++ models/Interview.js
@@ -8,7 +8,7 @@ const InterviewSchema=new mongoose.Schema({
     },
     user:{
         type:mongoose.Schema.ObjectId,
-        ref:'user',
+        ref:'User',
         required:true
     },
     company:{
